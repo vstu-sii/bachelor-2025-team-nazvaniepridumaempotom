4666c22077e9c98201622ad4810d29338f216d56
kokeruu <katyagulyaeva04@gmail.com>
2026-01-19 07:01:19 +0300

    CI Tests

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -6,7 +6,7 @@ const createJestConfig = nextJest({
 
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jest-environment-jsdom',  
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
